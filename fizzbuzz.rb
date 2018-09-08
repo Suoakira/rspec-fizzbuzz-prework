dc075509c87fab3cf065a61a6db467c7a56ae97b
Stephen Courtier <steves0089@gmail.com>
2018-09-08 09:17:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(x)
   if x % 3 == 0 && x % 5 == 0 
     return "FizzBuzz"
-  elsif x % 5
+  elsif x % 5 == 0
     return "Buzz"
-  elsif x % 3
+  elsif x % 3 == 0
     return "Fizz"
   end
 end
